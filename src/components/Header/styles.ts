e46d59f85c42f7c95f3271cb624727667a72e813
viktorhugodev <viktorhugo.dev@gmail.com>
2023-03-24 14:49:54 -0300

    feat: header and styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,3 +7,28 @@ export const HeaderContainer = styled.header`
   justify-content: space-between;
   background-color: #fafafa;
 `
+export const CartContainer = styled.div`
+  height: 38px;
+  width: 38px;
+  background-color: ${(props) => props.theme['yellow-light']};
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+export const Location = styled.div`
+  padding: 0.5rem;
+  gap: 4px;
+  height: 38px;
+  background-color: ${(props) => props.theme['purple-light']};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+`
+export const LocationCartContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+`
