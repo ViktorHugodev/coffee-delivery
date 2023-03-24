93d08fb0ff9b138cb39505a5c109ace7f71e7ac7
viktorhugodev <viktorhugo.dev@gmail.com>
2023-03-24 14:29:23 -0300

    feat: initial config, styles default

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  padding: 2rem 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fafafa;
+`
